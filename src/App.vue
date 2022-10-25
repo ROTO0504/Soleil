bee16e6b45dda8ffeab25416f6373e1ca38c77b9
ROTO0504 <0504hiroosa@gmail.com>
2022-10-25 22:27:51 +0900

    メニューバーの追加

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,12 @@
   <v-app>
     <v-container>
     <!-- スタイルは本記事のメインではないので雑に設定しています。この辺りもお好みで！ -->
-      <v-app-bar color="#FFFFFF" flat max-height="100" class="mb-5">
-        <v-toolbar-title>
-          <h1><router-link to="/" class="logo">ブログ！</router-link></h1>
-        </v-toolbar-title>
-
         <v-spacer></v-spacer>
 
         <div id="nav">
           <router-link to="/">Home</router-link> |
           <router-link to="/about">About</router-link>
         </div>
-      </v-app-bar>
       <router-view/>
     </v-container>
 <v-navigation-drawer>
---
+++ src/components/NavList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav class="nav-list-wrapper">
+      <ul class="nav-list">
+        <li class="nav-list-item"><a href="/">ホーム</a></li>
+        <li class="nav-list-item"><a href="/about">About</a></li>
+        <li class="nav-list-item"><a href="#">waiting...</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<style>
+.nav-list-wrapper {
+  padding: 10px;
+}
+.nav-list-item {
+  list-style: none;
+  padding: 8px 0;
+}
+.nav-list-item a {
+  color: #40b983;
+  text-decoration: none;
+  display: inline-block;
+  width: 100%;
+  font-size: 16px;
+  font-weight: 700;
+}
+.nav-list-item a:hover {
+  background: #40b983;
+  opacity: 0.8;
+  color: #fff;
+}
+</style>
